391e71988ac133bfa92b8bda13c0e2266e89ed56
kunal srivastav <shrivastavkunal56@gmail.com>
2025-08-11 08:45:31 -0700

    Ready for production

--- backend/utlis/cookieOptions.js
+++ backend/utlis/cookieOptions.js
@@ -1,7 +1,7 @@
 
 const isProduction = process.env.NODE_ENV === "production";
 
-module.exports.cookieOptions = {
+export const cookieOptions = {
   accessToken: {
     httpOnly: true,
     secure: isProduction,
